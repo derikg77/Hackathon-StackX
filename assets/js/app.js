b5cbe59af983b3b77ed8a3532ff4963e00a2ed56
Derik Gonçalves <derikadrianog@gmail.com>
2024-07-21 02:23:47 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -92,17 +92,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
     validateForm();
 })
 
- fetch('./assets/php/salvar.php', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ nome, email, mensagem }),
-})
-.then(response => response.json())
-.then(data => {
-    alert(data.message);
-})
-.catch((error) => {
-    console.error('Erro:', error);
-});
+//  fetch('./salvar.php', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ nameInput, whatsappInput, messageInput }),
+// })
+// .then(response => response.json())
+// .then(data => {
+//     alert(data.messageInput);
+// })
+// .catch((error) => {
+//     console.error('Erro:', error);
+// });
